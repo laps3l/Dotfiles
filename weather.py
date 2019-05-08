763ab2246ec8834f332e1a4cbf80aa53de836721
laps3 <36902075+laps3l@users.noreply.github.com>
2019-05-07 22:57:31 -0300

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,12 @@
+#!/bin/python3
+import urllib.request
+
+city = ""
+api_key = ""
+
+weather = eval(str(urllib.request.urlopen("http://api.openweathermap.org/data/2.5/weather?q={}&APPID={}".format(city, api_key)).read())[2:-1])
+
+info = weather["weather"][0]["description"].capitalize()
+temp = int(float(weather["main"]["temp"]) - 272.15)
+
+print("%s, %i°C" % (info, temp))
